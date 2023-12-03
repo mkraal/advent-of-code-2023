df5b39715b0fb29c009d0ae9fcf61734a78d6034
Michal Kral <michalkraal@gmail.com>
2023-12-03 21:37:17 +0100

    day 3

--- 2023-12-03/part1.py
+++ 2023-12-03/part1.py
@@ -1,1 +1,35 @@
+import re
 
+def main():
+    with open('2023-12-03/input.txt') as f:
+        raw_input = f.read()
+    special_chars = set(re.findall(r'[^\.\d\n]', raw_input)) # look for all unique special characters
+    input = raw_input.split('\n')
+    sum = 0
+    for i in range(len(input)):
+        for match in re.finditer(r'(\d+)', input[i]):
+            check_start = 0 if match.start() == 0 else match.start()-1
+            check_end = len(input[i]) if match.end() == len(input[i]) else match.end()+1
+            compare_current = list(input)[i][check_start: check_end]
+            if i == 0:
+                # first line only looks current and down
+                compare_below = list(input)[i+1][check_start: check_end]
+                if any(x in special_chars for x in compare_below) or any(x in special_chars for x in compare_current):
+                    print(f"{match.group()}")
+                    sum += int(match.group())
+            elif i == len(input)-1:
+                # last line only looks current and up
+                compare_above = list(input)[i-1][check_start: check_end]
+                if any(x in special_chars for x in compare_above) or any(x in special_chars for x in compare_current):
+                    print(f"{match.group()}")
+                    sum += int(match.group())
+            else:
+                # all other lines look everywhere
+                compare_above = list(input)[i-1][check_start: check_end]
+                compare_below = list(input)[i+1][check_start: check_end]
+                if any(x in special_chars for x in compare_above) or any(x in special_chars for x in compare_below) or any(x in special_chars for x in compare_current):
+                    print(f"{match.group()}")
+                    sum += int(match.group())
+    print(sum)
+if __name__ == '__main__':
+    main()
--- 2023-12-03/part2.py
+++ 2023-12-03/part2.py
@@ -1,1 +1,82 @@
+import re
 
+
+def get_coordinates(input):
+    "Returns all occurences of star signs and numbers with their coordinates"
+    stars = []
+    numbers = []
+    for i in range(len(input)):
+        for match in re.finditer(r"(\d+)", input[i]):
+            numbers.append(
+                {
+                    "row": i,
+                    "value": match.group(),
+                    "start": match.start(),
+                    "end": match.end(),
+                }
+            )
+        for j in range(len(input[i])):
+             if input[i][j] == "*":
+                stars.append([i, j])
+    return (stars, numbers)
+
+
+def is_below(star, number):
+    "Checks whether a number is below or diagonally down from a star"
+    left = number["start"] - 1
+    right = number["end"]
+    if (star[0] + 1 == number["row"]) and (left <= star[1] <= right):
+        return True
+    return False
+
+
+def is_above(star, number):
+    "Checks whether a number is above or diagonally up from a star"
+    left = number["start"] - 1
+    right = number["end"]
+    if star[0] - 1 == number["row"] and (left <= star[1] <= right):
+        return True
+    return False
+
+
+def is_row_neighbour(star, number):
+    "Checks whether a number is right next to a start (possible from both sides)"
+    if star[0] == number["row"] and (
+        star[1] == number["end"] or star[1] == number["start"]-1
+    ):
+        return True
+    return False
+
+
+def main():
+    with open("2023-12-03/input-p2.txt") as f:
+        input = f.read().split("\n")
+
+    sum = 0
+    star_value_combinations = []
+    coordinates = get_coordinates(input)
+    stars, numbers = coordinates[0], coordinates[1]
+    for star in stars:
+        # Iterate over map of stars
+        one_star_numbers = []
+        for number in numbers:
+            # Iterate over numbers and try to check whether they satisfy the conditions
+            if (
+                is_below(star, number)
+                or is_above(star, number)
+                or is_row_neighbour(star, number)
+            ):
+                # if they do, append them to a list of possible numbers
+                one_star_numbers.append(number)
+        # at the end, append the star - number neighbor combinations to a list
+        star_value_combinations.append({"star": star, "numbers": one_star_numbers})
+    print(star_value_combinations)
+    for star in star_value_combinations:
+        if len(star["numbers"]) == 2:
+            # If a star has exactly 2 neighbors, multiply them and add them to the sum
+            sum += int(star["numbers"][0]["value"]) * int(star["numbers"][1]["value"])
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
